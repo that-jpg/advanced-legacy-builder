7d7f801e00098ef1cb09a56725f11116f664ba9a
jpg <mbx.philipe@gmail.com>
2019-07-22 21:55:28 -0300

    starting project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to advanced-legacy-builder
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/advanced_legacy_builder/core_test.clj
@@ -0,0 +1,7 @@
+(ns advanced-legacy-builder.core-test
+  (:require [clojure.test :refer :all]
+            [advanced-legacy-builder.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
